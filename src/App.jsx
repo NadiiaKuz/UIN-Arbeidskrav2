2141311758266233bb9785d876543cc2ad23d577
NadiiaKuz <nadink1603@gmail.com>
2026-02-07 12:41:38 +0100

    added addform

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './style/handleliste.css'
+import AddForm from './components/AddForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const startItems = [
+    {prodId: 1, name: "Melk", amont: 2, bought: false},
+    {prodId: 2, name: "Egg", amont: 1, bought: true}
+  ]
+
+  const [items, setItems] = useState(startItems)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <section className='shoping-card'>
+        <header className='shopping-header'>
+          <h1>Handleliste</h1>
+        </header>
+
+        <AddForm />
+      
+      </section> 
+    </main>
   )
 }
 
---
+++ src/components/AddForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+export default function AddForm() {
+    const [nameInput, setNameInput] = useState("")
+    const [amountInput, setAmountInput] = useState("")
+
+    return (
+        <form className="addForm">
+            <label htmlFor="item-name">Vare</label>
+            <input 
+                id="item-name"
+                name="name"
+                type="text"
+                value={nameInput}
+                onChange={(e) => setNameInput(e.target.value)}
+                placeholder="Egg.."
+            />
+
+            <label htmlFor="item-amount">Antall</label>
+            <input
+                id="item-amount"
+                name="amount"
+                type="number"
+                min="1"
+                step={1}
+                value={amountInput}
+                onChange={(e) => setAmountInput(e.target.value)}
+                placeholder="2"
+            />
+
+            <button type="submit">Legg til vare</button>
+
+        </form>
+    )
+}
